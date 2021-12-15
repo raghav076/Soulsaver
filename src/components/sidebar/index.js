41a6efbb65a87679a46208bb0c312d69ae50da8a
Raghav Mukati <gurjarraghav2001@gmail.com>
2021-12-15 23:58:46 +0530

    push

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 import './sidebar.scss';
 
 const Sidebar = ({ onToggle }) => {
@@ -27,7 +28,7 @@ const Sidebar = ({ onToggle }) => {
 
     return (
         <div
-            style={{ width: `${!toggled ? '100%' : '50px'}` }}
+            style={{ width: `${!toggled ? '100%' : '50px'}`, minWidth: `${!toggled?'170px':'0px'}` }}
             className="sidebar__container"
         >
             <div className="icon__container">
@@ -39,6 +40,49 @@ const Sidebar = ({ onToggle }) => {
                 </div>
             </div>
             <div className="sectionBreak"></div>
+            <div className="sidebar__item__container">
+                <div className="sidebar__item__logo">
+                    <SearchIcon />
+                </div>
+                <div className={`sidebar__item__title ${toggled&&'none'}`}>
+                    Donors
+                </div>
+            </div>
+            <div className="sidebar__item__container">
+                <div className="sidebar__item__logo">
+                    <SearchIcon />
+                </div>
+                <div className={`sidebar__item__title ${toggled&&'none'}`}>
+                    Requestors
+                </div>
+            </div>
+            <div className="sidebar__item__container">
+                <div className="sidebar__item__logo">
+                <i className="fas fa-hand-holding-medical"></i>
+                </div>
+                <div className={`sidebar__item__title ${toggled&&'none'}`}>
+                    Donate Blood
+                </div>
+            </div>
+            <div className="sidebar__item__container">
+                <div className="sidebar__item__logo">
+                    <i class="fas fa-heartbeat"></i>
+                </div>
+                <div className={`sidebar__item__title ${toggled&&'none'}`}>
+                    Request Blood
+                </div>
+            </div>
+            <div className={`sidebar__item__container ${!toggled&&'none'}`}>
+                <div className="sidebar__item__logo">
+
+                </div>
+            </div>
+            <div className={`donation__item__container ${toggled&&'none'}`}>
+                <input type="number" placeholder="Enter Amount" />
+                <div className='sidebar__item__title'>
+                    <button>Donate</button>
+                </div>
+            </div>
         </div>
     )
 }
