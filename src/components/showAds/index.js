2422f2e4230b2456e3b258c354dd2a2690b533ea
Raghav Mukati <gurjarraghav2001@gmail.com>
2021-12-17 16:32:25 +0530

    filter bar

--- src/components/showAds/index.js
+++ src/components/showAds/index.js
@@ -3,6 +3,23 @@ import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArro
 import './showAds.scss';
 
 const ShowAds = () => {
+    const sampleAd = (
+        <ul className="ad__data">
+            <li className="name">Deepak Prasad</li>
+            <li className="phone">7610760240</li>
+            <li className="city">Asansol</li>
+            <li className="bgp">O +ve</li>
+            <li className="updated">5 min</li>
+            <li className="button"><button><KeyboardDoubleArrowRightIcon /></button></li>
+        </ul>
+    );
+    const renderAds = () => {
+        let result = [];
+        for (let i = 0; i < 10; i++) {
+            result.push(sampleAd);
+        }
+        return result;
+    }
     return (
         <div className="showAds__container">
             <div className="header__row">
@@ -18,24 +35,8 @@ const ShowAds = () => {
                     <li className="updated">Updated</li>
                     <li className="button"></li>
                 </ul>
-                {/* data */}
                 <div className="ads">
-                    <ul className="ad__data">
-                        <li className="name">Deepak Prasad</li>
-                        <li className="phone">7610760240</li>
-                        <li className="city">Asansol</li>
-                        <li className="bgp">O +ve</li>
-                        <li className="updated">5 min</li>
-                        <li className="button"><button><KeyboardDoubleArrowRightIcon /></button></li>
-                    </ul>
-                    <ul className="ad__data">
-                        <li className="name">Deepak Prasad</li>
-                        <li className="phone">7610760240</li>
-                        <li className="city">Asansol</li>
-                        <li className="bgp">O +ve</li>
-                        <li className="updated">5 min</li>
-                        <li className="button"><button><KeyboardDoubleArrowRightIcon /></button></li>
-                    </ul>
+                    {renderAds()}
                 </div>
             </div>
         </div>
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Filters from '../../components/filters';
 import Header from '../../components/header';
 import ShowAds from '../../components/showAds';
 import Sidebar from '../../components/sidebar';
+import stateData from '../../constants/states';
 import './home.scss';
 
 const Home = () => {
@@ -13,14 +14,19 @@ const Home = () => {
         setToggled(t);
     }
 
-    const [states, setStates] = useState([]);
+    const [states, setStates] = useState(stateData.map((s)=>s.state));
     const [district, setDistrict] = useState([]);
-    const [city, setCity] = useState([]);
+    // const [city, setCity] = useState([]);
     const [bloodGP, setBloodGP] = useState(['A +ve', 'B +ve', 'AB +ve', 'O +ve', 'A -ve', 'B -ve', 'AB -ve', 'O -ve']);
     const [selectedState, setSelectedState] = useState('');
     const [selectedDist, setSelectedDist] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
+    // const [selectedCity, setSelectedCity] = useState('');
     const [selectedBGP, setselectedBGP] = useState('');
+
+    useEffect(() => {
+        setSelectedDist('');
+        stateData.forEach((data, idx) => data.state === selectedState && setDistrict(stateData[idx].districts))
+    }, [selectedState])
 
     return (
         <>
@@ -36,22 +42,25 @@ const Home = () => {
                             <div className="filter__title">Filter Data:</div>
                             <Filters
                                 itemWidth={170}
+                                options={states}
                                 placeHolder='State'
                                 value={selectedState}
                                 handleChange={(value) => setSelectedState(value)} 
                             />
                             <Filters
                                 itemWidth={125}
+                                options={district}
                                 placeHolder='District' 
+                                inputVale={selectedDist}
                                 value={selectedDist}
                                 handleChange={(value) => setSelectedDist(value)} 
                             />
-                            <Filters
+                            {/* <Filters
                                 itemWidth={125}
                                 placeHolder='City'
                                 value={selectedCity}
                                 handleChange={(value) => setSelectedCity(value)} 
-                            />
+                            /> */}
                             <Filters
                                 itemWidth={115}
                                 placeHolder='Blood Group'
